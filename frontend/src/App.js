fce6d6d30438d6ee30c179837488ffe5961721af
ShreyaBayari-elsevier <s.bayari@elsevier.com>
2023-09-20 13:39:38 +0530

    adding navbar to login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar'; // Import Navbar component
-import Main from './pages/Main';
-import Login from './components/Login/Login'; // Import Login component
-import SignUp from './components/SignUp/SignUp'; // Import SignUp component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 // import AccountDetails from './components/Accounts/accountDetails'; // Import AccountDetails component
-// import AccountDetailsEdit from './components/accountDetailsEdit'; // Import Edit Account Details component
+// import AccountDetailsEdit from './components/Accounts/accountDetailsEdit'; // Import Edit Account Details component
+import Login from './components/Login/Login'; // Import Login component
+import NavBar from './components/NavBar/NavBar'; // Import Navbar component
+import NavBarHome from './components/NavBarHome/NavBarHome'; //Import Home Nav Bar
+import SignUp from './components/SignUp/SignUp'; // Import SignUp component
+import Main from './pages/Main';
 // import TransactionComponent from './components/Transaction/TransactionComponent'; // Import Transaction component
 
 
@@ -17,8 +18,8 @@ function App() {
         {/* Define routes for components */}
         <Routes> 
           <Route path="home" element={<Main />} />
-          <Route path="login" element={<div> <Login /> </div>} />
-          <Route path="signup" element={<div> <SignUp /> </div>} />
+          <Route path="login" element={<div> <NavBarHome/> <Login /> </div>} />
+          <Route path="signup" element={<div> <NavBarHome/> <SignUp /> </div>} />
           {/* <Route path="accounts" element={<div> <NavBar/><AccountDetails /> </div>} /> */}
           {/* <Route path="accountsedit" element={<div> <NavBar/><AccountDetailsEdit /> </div>} /> */}
           {/* <Route path="transactions" element={<div> <NavBar/> <TransactionComponent /> </div>} /> */}
