dfb4527c1eaecedf8c7d0a971659510df535bf77
ShreyaBayari-elsevier <s.bayari@elsevier.com>
2023-09-20 12:50:17 +0530

    adding account details to route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar/NavBar'; // Import Navbar component
 import Main from './pages/Main';
 import Login from './components/Login/Login'; // Import Login component
 import SignUp from './components/SignUp/SignUp'; // Import SignUp component
-// import AccountDetails from './components/Accounts/accountDetails'; // Import AccountDetails component
+import AccountDetails from './components/Accounts/accountDetails'; // Import AccountDetails component
 // import AccountDetailsEdit from './components/accountDetailsEdit'; // Import Edit Account Details component
 // import TransactionComponent from './components/Transaction/TransactionComponent'; // Import Transaction component
 
@@ -19,7 +19,7 @@ function App() {
           <Route path="home" element={<Main />} />
           <Route path="login" element={<div> <Login /> </div>} />
           <Route path="signup" element={<div> <SignUp /> </div>} />
-          {/* <Route path="accounts" element={<div> <NavBar/><AccountDetails /> </div>} /> */}
+          <Route path="accounts" element={<div> <NavBar/><AccountDetails /> </div>} />
           {/* <Route path="accountsedit" element={<div> <NavBar/><AccountDetailsEdit /> </div>} /> */}
           {/* <Route path="transactions" element={<div> <NavBar/> <TransactionComponent /> </div>} /> */}
         </Routes>
---
+++ frontend/src/components/Accounts/accountDetails.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './account-details.css';
+
+function AccountDetails() {
+  const [accountData, setAccountData] = useState({});
+  const accountId = "ACC002"; // Static ID
+
+  useEffect(() => {
+    // Fetch account details from API on page load
+    fetchAccountDetails(accountId);
+  }, [accountId]);
+
+  const fetchAccountDetails = (accountId) => {
+    axios.get(`http://localhost:8080/api/getaccount?id=${accountId}`)
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error('Network response was not ok');
+        }
+        return response.data;
+      })
+      .then((data) => {
+        setAccountData(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching account details:', error);
+      });
+  };
+
+  return (
+    <div className="container account-details">
+            <br/>    <br/>    <br/>    <br/>    <br/>
+      <div className="panel-body inf-content">
+        <div className="table-container">
+          <strong>Account Information</strong>
+          <table className="table table-user-information">
+            <tbody>
+            <tr>
+                    <td><strong>Account ID</strong></td>
+                    <td className="text-primary">{accountData.acc_id}</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Account Type</strong></td>
+                    <td className="text-primary">{accountData.acc_type}</td>
+                  </tr>
+                  <tr>
+                    <td><strong>First Name</strong></td>
+                    <td className="text-primary">{accountData.firstname}</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Second Name</strong></td>
+                    <td className="text-primary">{accountData.lastname}</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Phone Number</strong></td>
+                    <td className="text-primary">{accountData.phonum}</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Address</strong></td>
+                    <td className="text-primary">{accountData.address}</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Nominee</strong></td>
+                    <td className="text-primary">{accountData.nominee}</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Nationality</strong></td>
+                    <td className="text-primary">{accountData.nationality}</td>
+                  </tr>
+                  <tr>
+                    <td><strong>PAN Card Number</strong></td>
+                    <td className="text-primary">{accountData.pannum}</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Aadhar Number</strong></td>
+                    <td className="text-primary">{accountData.aadhar}</td>
+                  </tr>
+                  <tr>
+                    <td><strong>Balance</strong></td>
+                    <td className="text-primary">{accountData.balance}</td>
+                  </tr>
+            </tbody>
+          </table>
+          <button className="btn btn-primary edit-button">Edit Details</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AccountDetails;
+
