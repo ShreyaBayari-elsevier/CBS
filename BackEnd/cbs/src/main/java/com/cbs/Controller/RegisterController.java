e8fa73012b06bc54419bd136034ddf13b15ca313
tyagi-gopal <142214024+tyagi-gopal@users.noreply.github.com>
2023-09-21 12:29:12 +0530

    added the new method so that all can access the acc_id and use it further.

--- BackEnd/cbs/src/main/java/com/cbs/Controller/RegisterController.java
+++ BackEnd/cbs/src/main/java/com/cbs/Controller/RegisterController.java
@@ -3,9 +3,13 @@ package com.cbs.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cbs.Document.Register;
 import com.cbs.Service.RegisterService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/register")
 public class RegisterController {
 	  @Autowired
 	    private RegisterService service;
@@ -66,5 +71,17 @@ public class RegisterController {
   @DeleteMapping("/delete")
   public String deleteById(@RequestParam("id") String id) {
 	  return service.deleteById(id);
-  }  
+  }   
+   
+  @GetMapping("/login")
+	public ResponseEntity<Register> login(@RequestParam("id") String id, @RequestParam("pass") String pass) {
+	  Register register = service.login(id, pass);
+	  if(register!=null) {
+		  return new ResponseEntity<Register>(register,HttpStatus.OK);
+	  }
+	  else
+	  {
+		  return new ResponseEntity<Register>(register,HttpStatus.PARTIAL_CONTENT);
+	  }
+	}
 }
