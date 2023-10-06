08901e45f82e367fa3711be3c361005f0b6cd473
ShreyaBayari-elsevier <s.bayari@elsevier.com>
2023-10-06 13:03:03 +0530

    account details test

--- frontend/src/components/Navbar/NavBar.jsx
+++ frontend/src/components/Navbar/NavBar.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import './NavBar.css'; 
-import { Link, useNavigate } from 'react-router-dom'; 
+import { Link } from 'react-router-dom'; 
 
 function NavBar() {
-  const navigate = useNavigate();
-  const logoutHandler = () => {
-    localStorage.clear();
-    navigate('/');
-  }
+
   return (
     <div className="navbar">
       <img src='/logo.png' alt="Elsevier Logo" className="logo" />
@@ -22,10 +18,12 @@ function NavBar() {
         <Link to="/transactions" className='transaction-link'>
          <button type="submit" className='transaction'>TRANSACTION</button>
         </Link>
-            
-        <button type="submit" className='profile' onClick={logoutHandler}>
-          <img src="userProfile.png" alt="User Profile" className="user-profile" />
-        </button>
+        
+        <Link to="/profile" className='profile-link'>
+          <button type="submit" className='profile'>
+            <img src="userProfile.png" alt="User Profile" className="user-profile" />
+          </button>
+        </Link>
       </div>
     </div>
   );
