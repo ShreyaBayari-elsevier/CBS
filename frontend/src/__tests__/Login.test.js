c90617ebfa3e14a1a725a9f52de594ecef73a3b6
tyagi-gopal <g.tyagi@elsevier.com>
2023-10-09 10:13:28 +0530

    update

--- frontend/src/__tests__/Login.test.js
+++ frontend/src/__tests__/Login.test.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Login from '../components/Login/Login';
 
+describe('Test Suit',() => {
+
+
 test('renders login button', () => {
 //   render(<Login />);
     const screen = render(<MemoryRouter><Login/></MemoryRouter>);
@@ -10,3 +13,20 @@ test('renders login button', () => {
   const loginButton = screen.getByTestId('login-button');
   expect(loginButton).toBeInTheDocument();
 });
+
+// test('renders nothing if the button is not clicked',() => {
+//     const screen = render(<MemoryRouter><Login/></MemoryRouter>);
+//     const outputElement = screen.getByText('nothing',{exact: false});
+//     expect(outputElement).toBeInTheDocument();
+    test('renders login when button is not clicked', () => {
+        const screen = render(<MemoryRouter><Login/></MemoryRouter>);
+        
+        const loginHeader = screen.getByText('Login', { selector: 'h2' });
+        expect(loginHeader).toBeInTheDocument();
+        
+        const loginButton = screen.getByTestId('login-button');
+        expect(loginButton).toBeInTheDocument();
+      });
+      
+});
+// });
