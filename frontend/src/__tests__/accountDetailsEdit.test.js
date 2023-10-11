d0f24cf204f584f4bacdb40c71413360385d9a03
upadhyaya-hema <h.upadhyaya@elsevier.com>
2023-10-11 11:59:52 +0530

    test

--- frontend/src/__tests__/accountDetailsEdit.test.js
+++ frontend/src/__tests__/accountDetailsEdit.test.js
@@ -4,25 +4,96 @@ import React from 'react';
 import { MemoryRouter } from "react-router";
 import AccountDetails from "../components/Accounts/accountDetails";
 import AccountDetailsEdit from "../components/Accounts/accountDetailsEdit";
-// jest.mock('axios')
-// jest.mock('react-router-dom');
-// jest.mock('react-router-dom', () => ({
-//     ...jest.requireActual('react-router-dom'), // Use the actual module for everything else
-//     useNavigate: jest.fn(), // Mock the useNavigate hook
-//   }));
-
-// Mock localStorage and JSON.parse
-// const localStorageMock = {
-//     getItem: jest.fn(),
-//   };
-  
-//   global.localStorage = localStorageMock;
 
 describe('AccountDetailsEdit', () => {
+
+    test("check rendering of account number label",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const accNo_label = component.getByText('Account Number');
+        expect(accNo_label).toBeInTheDocument();
+    });
+
+    test("check rendering of phone number label",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const Phone_label=component.getByText("Phone Number");
+        expect(Phone_label).toBeInTheDocument();
+    });
+
+    test("check rendering of address label",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const address_label=component.getByText('Address');
+        expect(address_label).toBeInTheDocument();
+    });
+
+    test("check rendering of nominee label",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const nominee_label=component.getByText("Nominee");
+        expect(nominee_label).toBeInTheDocument();
+    });
+
+    test("check rendering of account input field",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const accId_input = component.getByTestId('acc_id');
+        expect(accId_input).toBeInTheDocument();
+    });
+
+    test("check rendering of phone number input field",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const phoneNum_input = component.getByTestId('phonum');
+        expect(phoneNum_input).toBeInTheDocument();
+    });
+
+    test("check rendering of address input field",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const address_input = component.getByTestId('address');
+        expect(address_input).toBeInTheDocument();
+    });
+
+    test("check rendering of nominee input field",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const nominee_input = component.getByTestId('nominee');
+        expect(nominee_input).toBeInTheDocument();
+    });
+
+    test("check account number placeholder",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const accNo_placeholder = component.getByPlaceholderText("Enter Account Number")
+        expect(accNo_placeholder).toBeInTheDocument();
+    });
+
+    test("check phone number placeholder",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const phoneNo_placeholder = component.getByPlaceholderText("Enter Phone Number")
+        expect(phoneNo_placeholder).toBeInTheDocument();
+    });
+
+    test("check address placeholder",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const address_placeholder = component.getByPlaceholderText("Enter Address")
+        expect(address_placeholder).toBeInTheDocument();
+    });
+
+    test("check nominee placeholder",()=>{
+        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
+
+        const nominee_placeholder = component.getByPlaceholderText("Enter Nominee Name")
+        expect(nominee_placeholder).toBeInTheDocument();
+    });
+
     test("on click of submit button redirect from edit details page to the view details page",()=>{
         const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
 
-        // const editbtn=screen.getByLabelText("subbtn");
         const editbtn=component.getByText("Submit")
         expect(editbtn).toBeInTheDocument();
         fireEvent.click(editbtn);
@@ -32,51 +103,7 @@ describe('AccountDetailsEdit', () => {
         const accinfo=component1.getByLabelText("Account_Information");
         expect(accinfo).toBeInTheDocument()
     });
-    test("check the rendering of labels and the input fields",()=>{
-        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
 
-        const accNo_label = component.getByText('Account Number');
-        expect(accNo_label).toBeInTheDocument();
-
-        const Phone_label=component.getByText("Phone Number");
-        expect(Phone_label).toBeInTheDocument();
-
-        const address_label=component.getByText('Address');
-        expect(address_label).toBeInTheDocument();
-
-        const nominee_label=component.getByText("Nominee");
-        expect(nominee_label).toBeInTheDocument();
-
-        const accId_input = component.getByTestId('acc_id');
-        expect(accId_input).toBeInTheDocument();
-
-        const phoneNum_input = component.getByTestId('phonum');
-        expect(phoneNum_input).toBeInTheDocument();
-
-        const address_input = component.getByTestId('address');
-        expect(address_input).toBeInTheDocument();
-
-        const nominee_input = component.getByTestId('nominee');
-        expect(nominee_input).toBeInTheDocument();
-
-        //expect(screen.getByLabelText('acc_no').getAttribute("placeholder")).toBe("Enter Account Number")
-    });
-    test("testing placeholders",()=>{
-        const component=render(<MemoryRouter><AccountDetailsEdit/></MemoryRouter>);
-
-        const accNo_placeholder = component.getByPlaceholderText("Enter Account Number")
-        expect(accNo_placeholder).toBeInTheDocument(); 
-
-        const phoneNo_placeholder = component.getByPlaceholderText("Enter Phone Number")
-        expect(phoneNo_placeholder).toBeInTheDocument(); 
-
-        const address_placeholder = component.getByPlaceholderText("Enter Address")
-        expect(address_placeholder).toBeInTheDocument(); 
-
-        const nominee_placeholder = component.getByPlaceholderText("Enter Nominee Name")
-        expect(nominee_placeholder).toBeInTheDocument(); 
-
-    });
     test("test the submit button when updation fails",async()=>{
             window.alert = jest.fn()
             axios.put = jest.fn().mockImplementation(()=>{
@@ -90,7 +117,6 @@ describe('AccountDetailsEdit', () => {
             expect(window.alert).toHaveBeenCalledWith("Updation failed");
         })
     });
-
     
     test("test the submit button for successful updation",async()=>{
         window.alert=jest.fn();
