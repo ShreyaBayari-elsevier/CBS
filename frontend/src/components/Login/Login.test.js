0b5e2d374ce61f21f61c263edfd30dc26080ca11
tyagi-gopal <g.tyagi@elsevier.com>
2023-10-11 12:07:23 +0530

    updated the indentation

--- frontend/src/components/Login/Login.test.js
+++ frontend/src/components/Login/Login.test.js
@@ -3,30 +3,23 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Login from '../Login/Login';
 
-describe('Test Suit',() => {
+describe('Test Suit', () => {
 
-
-test('renders login button', () => {
-//   render(<Login />);
+  
+  test('renders login button', () => {
     const screen = render(<MemoryRouter><Login/></MemoryRouter>);
 
-  const loginButton = screen.getByTestId('login-button');
-  expect(loginButton).toBeInTheDocument();
-});
+    const loginButton = screen.getByTestId('login-button');
+    expect(loginButton).toBeInTheDocument();
+  });
 
-// test('renders nothing if the button is not clicked',() => {
-//     const screen = render(<MemoryRouter><Login/></MemoryRouter>);
-//     const outputElement = screen.getByText('nothing',{exact: false});
-//     expect(outputElement).toBeInTheDocument();
-    test('renders login when button is not clicked', () => {
-        const screen = render(<MemoryRouter><Login/></MemoryRouter>);
+  test('renders login when the button is not clicked', () => {
+    const screen = render(<MemoryRouter><Login/></MemoryRouter>);
         
-        const loginHeader = screen.getByText('Login', { selector: 'h2' });
-        expect(loginHeader).toBeInTheDocument();
+    const loginHeader = screen.getByText('Login', { selector: 'h2' });
+    expect(loginHeader).toBeInTheDocument();
         
-        const loginButton = screen.getByTestId('login-button');
-        expect(loginButton).toBeInTheDocument();
-      });
-      
+    const loginButton = screen.getByTestId('login-button');
+    expect(loginButton).toBeInTheDocument();
+  });
 });
-// });
